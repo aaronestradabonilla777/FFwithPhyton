4f0a6e3a3d9d55fa1cde741e5a52936ffe6416a2
aaronestradabonilla777 <lacomarcaz@hotmail.com>
2024-01-05 19:54:25 -0600

    Add methods for leveling up, checking if the character is alive, dying, and attacking

--- phyton002/FFPython/app/main.py
+++ phyton002/FFPython/app/main.py
@@ -13,6 +13,32 @@ class Personaje:
         print("Vida:", self.vida)
         print("Inteligencia:", self.inteligencia)
         print("Defensa:", self.defensa)
+# def subir_nivel es para subir los atributos del personaje
+def subir_nivel(self):
+    self.fuerza = self.fuerza + fuerza
+    self.inteligencia = self.inteligencia + inteligencia
+    self.defensa = self.defensa + defensa
+
+#esta_vivo es para saber si el personaje esta vivo o no
+def esta_vivo(self):
+    return self.vida > 0
+#def morir es para que el personaje muera
+def morir(self):
+    self.vida = 0
+    print("El personaje ha muerto")
+
+def daño(self, enemigo):
+    return self.fuerza - enemigo.defensa
+
+def atacar(self, enemigo):
+    daño = self.daño(enemigo)
+    enemigo.vida = enemigo.vida - daño
+    print(self.nombre, "ha realizado", daño, "puntos de daño a", enemigo.nombre)
+    print("La vida de", enemigo.nombre, "es", enemigo.vida)
+
 
 mi_personaje = Personaje("Arthur", 10, 100, 5, 5)
 mi_personaje.atributos()
+
+
+
