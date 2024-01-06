e2fde4d7d7a49642cf0c97fff06432a08bdbef5f
aaronestradabonilla777 <lacomarcaz@hotmail.com>
2024-01-05 20:17:26 -0600

    Refactor character class and add new words to spell check

--- phyton002/FFPython/app/main.py
+++ phyton002/FFPython/app/main.py
@@ -2,51 +2,52 @@
 class Personaje:
     #def __init__ es para inicializar los atributos del personaje
     def __init__(self, nombre, fuerza, vida, inteligencia, defensa):
-        self.nombre = nombre
-        self.fuerza = fuerza
-        self.vida = vida
-        self.inteligencia = inteligencia
-        self.defensa = defensa
+        self.__nombre = nombre
+        self.__fuerza = fuerza
+        self.__vida = vida
+        self.__inteligencia = inteligencia
+        self.__defensa = defensa
  #def atributos es para mostrar los atributos del personaje
     def atributos(self):
-        print(self.nombre, ":", sep="")
-        print("Fuerza:", self.fuerza)
-        print("Vida:", self.vida)
-        print("Inteligencia:", self.inteligencia)
-        print("Defensa:", self.defensa)
+        print(self.__nombre, ":", sep="")
+        print("Fuerza:", self.__fuerza)
+        print("Vida:", self.__vida)
+        print("Inteligencia:", self.__inteligencia)
+        print("Defensa:", self.__defensa)
 
 
 # def subir_nivel es para subir los atributos del personaje
 def subir_nivel(self , fuerza, inteligencia, defensa):
-    self.fuerza = self.fuerza + fuerza
-    self.inteligencia = self.inteligencia + inteligencia
-    self.defensa = self.defensa + defensa
+    self.__fuerza = self.__fuerza + fuerza
+    self.__inteligencia = self.__inteligencia + inteligencia
+    self.__defensa = self.__defensa + defensa
 
 #esta_vivo es para saber si el personaje esta vivo o no
 def esta_vivo(self):
-    return self.vida > 0
+    return self.__vida > 0
+
 #def morir es para que el personaje muera
 def morir(self):
-    self.vida = 0
+    self.__vida = 0
     print("El personaje ha muerto")
 
+ #def daño es para calcular el daño que hace el personaje
 def daño(self, enemigo):
-    return self.fuerza - enemigo.defensa
+    return self.__fuerza - enemigo.__fuerza
 
+ #def atacar es para que el personaje ataque al enemigo
 def atacar(self, enemigo):
     daño = self.daño(enemigo)
-    enemigo.vida = enemigo.vida - daño
-    print(self.nombre, "ha realizado", daño, "puntos de daño a", enemigo.nombre)
-    if enemigo.estavivo():
-       print("La vida de", enemigo.nombre, "es", enemigo.vida)
+    enemigo.__vida = enemigo.__vida - daño
+    print(self.__nombre, "ha realizado", daño, "puntos de daño a", enemigo.__nombre)
+    if enemigo.esta_vivo():
+       print("La vida de", enemigo.__nombre, "es", enemigo.__vida)
     else:
         enemigo.morir()
 
 
 mi_personaje = Personaje("Arthur", 10, 100, 5, 5)
-mi_enemigo = Personaje("Mordred", 8, 100, 5, 3)
-mi_personaje.atacar(mi_enemgio)
-mi_personaje.atributos()
 
+mi_enemigo = Personaje("Mordred", 8, 100, 5, 3)
 
 
