d42428f4ded7d5584e31f3c36663f757b7678dea
aaronestradabonilla777 <lacomarcaz@hotmail.com>
2024-01-05 19:16:02 -0600

    Add Personaje class to main.py

--- java002/FFPython/app/main.py
+++ java002/FFPython/app/main.py
@@ -1,5 +1,19 @@
 def main():
     print("Hola, mundo!")
 
-if __name__ == "__main__":
-    main()
+class Personaje :
+    nombre = "Arthur"
+    fuerza = 10
+    vida = 100
+    inteligencia = 16
+    destreza = 8
+    defensa = 12
+
+
+mi_personaje = Personaje()
+print("El nombre del personaje es", mi_personaje.nombre)
+print("La fuerza del personaje es", mi_personaje.fuerza)
+print("La vida del personaje es", mi_personaje.vida)
+print("La inteligencia del personaje es", mi_personaje.inteligencia)
+print("La destreza del personaje es", mi_personaje.destreza)
+print("La defensa del personaje es", mi_personaje.defensa)
